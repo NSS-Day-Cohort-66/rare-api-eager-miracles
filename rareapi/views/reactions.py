b6803c07f2fabb5f7e2b11d1f701e623eebef6f9
Nicholas Williams <nicholaswilliams910@gmail.com>
2023-11-20 16:58:44 -0500

    added reaction serializer, reactionviewset, and list, retrieve, update, and create methods

--- rareapi/views/reactions.py
+++ rareapi/views/reactions.py
@@ -7,7 +7,7 @@ class ReactionSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Reaction
-        fields = ['label', 'image_url']
+        fields = ['id', 'label', 'image_url']
 
 class ReactionViewSet(viewsets.ViewSet):
 
@@ -21,5 +21,34 @@ class ReactionViewSet(viewsets.ViewSet):
             reaction = Reaction.objects.get(pk=pk)
             serializer = ReactionSerializer(reaction, context={'request': request})
             return Response(serializer.data)
+        except Reaction.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        
+    def create(self, request):
+        label = request.data.get('label')
+        image_url = request.data.get('image_url')
+
+        reaction = Reaction.objects.create(
+            label=label,
+            image_url=image_url
+        )
+
+        serializer = ReactionSerializer(reaction, context={'request': request})
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def update(self, request, pk=None):
+        try:
+            reaction = Reaction.objects.get(pk=pk)
+
+            serializer = ReactionSerializer(data=request.data)
+            if serializer.is_valid():
+                reaction.label = serializer.validated_data['label']
+                reaction.image_url = serializer.validated_data['image_url']
+                reaction.save()
+
+                serializer = ReactionSerializer(reaction, context={'request': request})
+                return Response(None, status.HTTP_204_NO_CONTENT)
+            
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         except Reaction.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
