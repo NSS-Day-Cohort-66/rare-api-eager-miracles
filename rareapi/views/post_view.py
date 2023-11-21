169a4dfdc3213c7717f94cef59f55de835186e46
Michelle Primiani <primianimichelle@gmail.com>
2023-11-21 09:41:57 -0600

    Updated image urls in posts

--- rareapi/views/post_view.py
+++ rareapi/views/post_view.py
@@ -4,7 +4,6 @@ from rest_framework.response import Response
 from rest_framework import serializers, status
 from rareapi.models import Post, Category, RareUser, PostTag
 from django.contrib.auth.models import User
-from datetime import datetime
 
 
 class PostUserSerializer(serializers.ModelSerializer):
