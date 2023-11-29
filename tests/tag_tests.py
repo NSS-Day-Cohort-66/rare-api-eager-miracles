4217d6af7923842a3f8b26fea6af875a0a0b30d8
Spencer Norris <Spencer.serv@gmail.com>
2023-11-29 11:34:46 -0600

    tests for tags completed

--- tests/tag_tests.py
+++ tests/tag_tests.py
@@ -29,3 +29,47 @@ class TagTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         
         self.assertEqual(json_response["label"], "Nick")
+        
+    def test_get_tag(self):
+        tag = Tag()
+        tag.label = "Nick"
+        
+        tag.save()
+        
+        response = self.client.get(f"/tags/{tag.id}")
+        
+        json_response = json.loads(response.content)
+        
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+        self.assertEqual(json_response["label"], "Nick")
+
+    def test_change_tag(self):
+        
+        tag = Tag()
+        tag.label = "Nick"
+        tag.save()
+        
+        data = {
+            "label": "Luke"
+        }
+        
+        response = self.client.put(f"/tags/{tag.id}", data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        
+        response = self.client.get(f"/tags/{tag.id}")
+        json_response = json.loads(response.content)
+        
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(json_response["label"], "Luke")
+
+    def test_delete_tag(self):
+        tag = Tag()
+        tag.label = "Nick"
+        tag.save()
+        
+        response = self.client.delete(f"/tags/{tag.id}")
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        
+        response = self.client.get(f"/tags/{tag.id}")
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
